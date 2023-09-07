2d4c3e09f255f9a74603f95efdd90405289011a7
warzes <warzes@mail.ru>
2023-09-07 17:18:21 +1000

    - проект

---
+++ src/Game/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "TestApp.h"
+
+в дискорде найти raycasterpro
+//-----------------------------------------------------------------------------
+#if defined(_MSC_VER)
+#	pragma comment( lib, "Engine.lib" )
+#endif
+//-----------------------------------------------------------------------------
+int main(
+	[[maybe_unused]] int   argc,
+	[[maybe_unused]] char* argv[])
+{
+	EngineDeviceCreateInfo createInfo;
+	createInfo.window.maximized = false;
+	//createInfo.window.width = 1280;
+	//createInfo.window.height = 960;
+	createInfo.window.vsyncEnabled = true;
+	auto engineDevice = EngineDevice::Create(createInfo);
+	engineDevice->RunApp(std::make_shared<TestApp>());
+}
+//-----------------------------------------------------------------------------
